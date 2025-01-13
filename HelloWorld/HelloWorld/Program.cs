68a8f9209cd37bf4f9c67a478b6c71898dce4359
ismaildedec <ddcismail@gmail.com>
2025-01-13 22:49:31 +0300

    Console.Write,WriteLine,Read,ReadKey

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -6,7 +6,33 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            #region Giris
+            /*Console.WriteLine("Hello World!");
+            
+                Console.WriteLine("Merhaba Dünya");
+                Her Satır ; ile biter
+             */
+
+            #endregion
+
+            #region Console.Write,WriteLine,Read,ReadKey
+            /*
+              Console.Write("Benim adim: ");  //  Console.Write alt satıra geçmez 
+               Console.WriteLine("ismail");    //  Console.WriteLine alt  satıra geçer.
+               Console.Write("Soyadim: ");
+               Console.WriteLine("dedeç");
+               Console.Read();                 // Enter tusuna basmadan diger kodlara gecmez bekler.
+               Console.Write("deneme, ");
+               Console.Write("deneme01, ");
+               Console.Write("deneme02, ");
+
+               Console.WriteLine();
+               Console.ReadKey();                 // Klavyeden herhangi bir tusa tusuna basmadan diger kodlara gecmez bekler.
+               Console.WriteLine("deneme, ");
+               Console.WriteLine("deneme01, ");
+               Console.WriteLine("deneme02, ");
+              */
+            #endregion
         }
     }
 }
