567a46c14a7fbb272f5e7710aa75d0767a843061
ismaildedec <ddcismail@gmail.com>
2025-02-24 22:38:50 +0300

    constants added.

---
+++ Windows_Form_App/WFA/WFA/constants.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFA
+{
+    public partial class constants : Form
+    {
+        public constants()
+        {
+            InitializeComponent();
+        }
+        const double pi = 3.141;
+        private void constants_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double pi = 3;   //-> const performans kazandırır cunku ram de daha az yer kaplar. 
+        }
+    }
+}
