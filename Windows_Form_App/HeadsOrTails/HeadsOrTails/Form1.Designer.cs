23253c583c63da47216a1c82707ab32f09c72ff6
ismaildedec <ddcismail@gmail.com>
2025-03-04 00:52:58 +0300

    Coin toss game with do while method.

---
+++ Windows_Form_App/HeadsOrTails/HeadsOrTails/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace HeadsOrTails
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTahmin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnTahmin
+            // 
+            this.btnTahmin.Location = new System.Drawing.Point(296, 155);
+            this.btnTahmin.Name = "btnTahmin";
+            this.btnTahmin.Size = new System.Drawing.Size(169, 74);
+            this.btnTahmin.TabIndex = 0;
+            this.btnTahmin.Text = "Tahmin Et";
+            this.btnTahmin.UseVisualStyleBackColor = true;
+            this.btnTahmin.Click += new System.EventHandler(this.btnTahmin_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnTahmin);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTahmin;
+    }
+}
+
